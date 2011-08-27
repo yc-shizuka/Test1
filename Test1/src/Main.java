8accade3a73adabcaee44352056085cdb3669937
Yoshihide Chubachi <yoshi@chubachi.net>
2011-08-27 10:09:37 +0900

    バグった

--- Test1/src/Main.java
+++ Test1/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("こんにちは．");
 		System.out.println("ありがとう．");
-		System.out.println("こんばんは"．");
+		System.out.println("こんばんは．");
 	}
 
 }
